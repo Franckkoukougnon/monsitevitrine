d80fd1ae1c4b9c7c75df20d8f93147590a40795b
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-05-21 19:21:27 +0200

    new component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,9 +6,8 @@ import "../style/Home.css";
 function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
-      t
       <div className="headerContainer">
-        <h1> Mael Restaurant</h1>
+        <h1> Mael Pizzaria</h1>
         <p> Bienvenue dans notre restaurant </p>
         <Link to="/menu">
           <button> Order Now</button>
